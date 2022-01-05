de37e5f19331b16d4cdc279ac5a9af5b558c80f7
mariotmc <mariotomicx@gmail.com>
2022-01-05 07:12:40 +0100

    Add app components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import Header from "./components/Header";
+import Scoreboard from "./components/Scoreboard";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 import "./styles/style.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Scoreboard />
+        <Cards />
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;

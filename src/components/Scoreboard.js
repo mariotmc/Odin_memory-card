8c5515acf3a91b35c2b4a353f9d3047b2a128eea
mariotmc <mariotomicx@gmail.com>
2022-01-05 07:11:47 +0100

    Add scoreboard component

---
+++ src/components/Scoreboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Scoreboard = () => {
+  return (
+    <div className="scoreboard">
+      <div className="score current-score">Current Score: 0</div>
+      <div className="score best-score">Best Score: 0</div>
+    </div>
+  );
+};
+
+export default Scoreboard;

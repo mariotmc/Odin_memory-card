ec7daff2bbe79b48fc59adaefc29d2b2a4e29ab1
mariotmc <mariotomicx@gmail.com>
2022-01-06 04:48:37 +0100

    Add guessing & scores functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import Header from "./components/Header";
 import Scoreboard from "./components/Scoreboard";
 import Cards from "./components/Cards";
@@ -6,11 +6,41 @@ import Footer from "./components/Footer";
 import "./styles/style.css";
 
 const App = () => {
+  const [guesses, setGuesses] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highscore, setHighscore] = useState(0);
+
+  const guessing = useCallback(
+    (e) => {
+      let newGuess = e.target.parentNode.children[1].innerText;
+      if (!guesses.includes(newGuess)) {
+        setGuesses([...guesses, newGuess]);
+        setScore(score + 1);
+      } else if (guesses.includes(newGuess)) {
+        setGuesses([]);
+        setScore(0);
+      }
+    },
+    [guesses, score]
+  );
+
+  useEffect(() => {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((element) => element.addEventListener("click", guessing));
+
+    if (score > highscore) setHighscore(score);
+
+    return () => {
+      cards.forEach((element) => element.removeEventListener("click", guessing));
+    };
+  }, [guessing, highscore, score]);
+
   return (
     <>
       <Header />
       <main>
-        <Scoreboard />
+        <Scoreboard score={score} highscore={highscore} />
         <Cards />
       </main>
       <Footer />

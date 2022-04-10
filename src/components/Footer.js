83304b163a059170eb8d27855b60fe6563151b15
mariotmc <mariotomicx@gmail.com>
2022-04-10 16:58:46 +0200

    Add back to top of the page link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,20 @@
-import React from "react";
-
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className="relative flex justify-center">
+      <a href="#top" className="absolute -top-3 bg-customGray p-1 rounded-full hover:animate-bounce">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-8 w-8"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="white"
+          strokeWidth="3"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M5 11l7-7 7 7M5 19l7-7 7 7" />
+        </svg>
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;

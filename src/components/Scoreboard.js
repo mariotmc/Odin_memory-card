c986ddd389268548f7b6c9beeba98d662697b0af
mariotmc <mariotomicx@gmail.com>
2022-04-10 17:22:16 +0200

    Add button that brings up tutorial modal

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,7 +1,14 @@
+import { QuestionMarkCircleIcon } from "@heroicons/react/outline";
+
 const Scoreboard = (props) => {
   return (
     <div className="scoreboard">
       <div className="score current-score">Current Score: {props.score}</div>
+      <QuestionMarkCircleIcon
+        stroke="#383838"
+        className="w-[46px] h-[46px] cursor-pointer"
+        onClick={() => props.setOpen(true)}
+      />
       <div className="score best-score">Best Score: {props.highscore}</div>
     </div>
   );
